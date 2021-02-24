abc9083fedf870eab01a827e4b93980d7340d007
frameDoubt <brandon.w.wright4@gmail.com>
2021-02-24 15:51:15 -0800

    add cancel function

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -15,6 +15,12 @@ export default function Form(props) {
     event.preventDefault();
   }
 
+  const cancel = function() {
+    setValue('');
+    setInterviewer(null);
+    props.onCancel();
+  }
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
@@ -36,7 +42,7 @@ export default function Form(props) {
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger onClick={props.onCancel}>Cancel</Button>
+          <Button danger onClick={cancel}>Cancel</Button>
           <Button confirm onClick={props.onSave}>Save</Button>
         </section>
       </section>
