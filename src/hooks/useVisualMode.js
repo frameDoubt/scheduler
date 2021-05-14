b86815a0898e487a528bc494e766764f967b5579
frameDoubt <brandon.w.wright4@gmail.com>
2021-05-13 22:49:01 -0700

    add transition function to custom hook

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -3,6 +3,7 @@ import { useState } from 'react';
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   return {
-    mode
+    mode,
+    transition(second) { setMode(second) }
   };
 };
