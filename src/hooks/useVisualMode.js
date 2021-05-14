83a78dcec4319f7bcdabb832740b8f464c892f17
frameDoubt <brandon.w.wright4@gmail.com>
2021-05-13 23:02:02 -0700

    add back function to useVisalMode hook

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,8 +2,13 @@ import { useState } from 'react';
 
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
+
+  function transition(second) { setMode(second) }
+  function back() {}
+
   return {
     mode,
-    transition(second) { setMode(second) }
+    transition,
+    back
   };
 };
