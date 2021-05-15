03f442791ba1aafaf4ff8e4a337acc330dd518a7
frameDoubt <brandon.w.wright4@gmail.com>
2021-05-14 18:37:01 -0700

    add useVisualMode hook

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,10 +2,27 @@ import { useState } from 'react';
 
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
 
-  function transition(second) { setMode(second) }
-  function back() {}
-
+  function transition(second, boolParam) {
+    if(!boolParam) {
+      setHistory((prev) => {
+        return [...prev, second]
+      });
+      setMode(second);
+    } else {
+      setMode(second);
+    }
+  };
+  function back() {
+    if(history.length > 1) {
+      setHistory((prev) => {
+        prev.pop();
+        setMode(prev[prev.length - 1]);
+        return prev;
+      });
+    }
+  };
   return {
     mode,
     transition,
