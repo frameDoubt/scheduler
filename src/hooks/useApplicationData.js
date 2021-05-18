3b7599f1d31f502e8f715e4c7b289f2ba6782f65
frameDoubt <brandon.w.wright4@gmail.com>
2021-05-18 15:57:05 -0700

    add getSpotsForDay function and fix but with cancelInterview function

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -5,48 +5,48 @@ export default function UseApplicationData (props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments:
-    [
-      {
-        id: 1,
-        time: "12pm"
-      },
-      {
-        id: 2,
-        time: "1pm",
-        interview: {
-          student: "Lydia Miller-Jones",
-          interviewer: {
-            id: 1,
-            name: "Sylvia Palmer",
-            avatar: "https://i.imgur.com/LpaY82x.png"
-          }
-        }
-      },
-      {
-        id: 3,
-        time: "2pm"
-      },
-      {
-        id: 4,
-        time: "3pm",
-        interview: {
-          student: "Jessica Jones",
-          interviewer: {
-            id: 5,
-            name: "Sven Jones",
-            avatar: "https://i.imgur.com/twYrpay.jpg"
-          }
-        }
-      },
-      {
-        id: 5,
-        time: "4pm"
-      }
-    ]
+    // appointments:
+    // [
+    //   {
+    //     id: 1,
+    //     time: "12pm"
+    //   },
+    //   {
+    //     id: 2,
+    //     time: "1pm",
+    //     interview: {
+    //       student: "Lydia Miller-Jones",
+    //       interviewer: {
+    //         id: 1,
+    //         name: "Sylvia Palmer",
+    //         avatar: "https://i.imgur.com/LpaY82x.png"
+    //       }
+    //     }
+    //   },
+    //   {
+    //     id: 3,
+    //     time: "2pm"
+    //   },
+    //   {
+    //     id: 4,
+    //     time: "3pm",
+    //     interview: {
+    //       student: "Jessica Jones",
+    //       interviewer: {
+    //         id: 5,
+    //         name: "Sven Jones",
+    //         avatar: "https://i.imgur.com/twYrpay.jpg"
+    //       }
+    //     }
+    //   },
+    //   {
+    //     id: 5,
+    //     time: "4pm"
+    //   }
+    // ]
   });
 
-  const setDay = day => setState({ ...state, day });
+  const setDay = day => setState((prev) => {return {...prev, day}});
 
   function bookInterview(id, interview) {
     const appointment = {
@@ -57,24 +57,58 @@ export default function UseApplicationData (props) {
       ...state.appointments,
       [id]: appointment
     };
-    setState({
-      ...state,
-      appointments
+
+    // console.log(state.days[id].spots);
+    const newDaysArr = updateSpots(state.day, state.days, appointments);
+
+    setState((prev) => {
+      return {...prev, appointments, days: newDaysArr}
     });
     return axios.put(`/api/appointments/${id}`, {interview})
   }
 
   function cancelInterview(id) {
-    console.log("cancelInterview idParam:", id);
+    
     const appointment = {
       ...state.appointments[id],
       interview: null
     };
-    setState({
-      ...state,
-      appointment
-    });
+    
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+    console.log("Appointments:", appointments);
+    const newDaysArr = updateSpots(state.day, state.days, appointments);
+    console.log("newDaysArr:", newDaysArr);
+    setState((prev) => {
+     return { ...prev, appointments, days: newDaysArr}
+    })
+    
     return axios.delete(`/api/appointments/${id}`)
+  }
+
+  function getSpotsForDay(dayObj, appointments) {
+    let spots = 0;
+    for(const id of dayObj.appointments) {
+      const appointment = appointments[id];
+      if(!appointment.interview) {
+        spots++;
+      }
+    }
+    return spots;
+  }
+
+  function updateSpots(dayName, days, appointments) {
+    // returns array with object with matching name obj.property
+    const dayObj = days.find(day => day.name === dayName);
+    // store returned value, contains number of appointments, 
+    // that have null value for appointment obj.property
+    const spots = getSpotsForDay(dayObj, appointments);
+    // new object for mapping to state obj
+    const newDay = { ...dayObj, spots }
+    const newDays = days.map(day => day.name === dayName ? newDay : day);
+    return newDays;
   }
 
   useEffect(() => {
@@ -87,6 +121,9 @@ export default function UseApplicationData (props) {
       setState(prev => ({ ...prev, days: days.data, appointments: appts.data, interviewers: interviewers.data }));
     });
   }, []);
-
+  // console.log(`These are sync state keys: ${Object.keys(state)}`);
+  // console.log(`Is state.days an array: ${ Array.isArray(state.days) }`);
+  // console.log(`This is state.days keys: ${Object.keys(state.days)}`);
+  console.log(`This is state:`, state);
   return { state, setState, setDay, bookInterview, cancelInterview };
 }
