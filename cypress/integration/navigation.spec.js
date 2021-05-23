bed4a5d214fcd3a0679d8b23e1a5438e2ff09213
frameDoubt <brandon.w.wright4@gmail.com>
2021-05-22 19:40:23 -0700

    add test to book an appointment

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -16,17 +16,28 @@ describe("Navigation", () => {
   });
 
   it("should book an interview", () => {
+
     // Visits the root of our web server
-    cy.visits("/");
+    cy.visit("/");
+
     // Clicks on the "Add" button in the second appointment
-    
+    cy.get(":nth-child(2) > .appointment__add > .appointment__add-button").click();
+
     // Enters their name
+    cy.get("[data-testid=student-name-input]").type("Jessica Jones");
 
     // Chooses an interviewer
+    cy.get(":nth-child(2) > .interviewers__item-image").click();
 
     // Clicks the save button
+    cy.get(".button--confirm").click();
 
     // Sees the booked appointment
+    cy.get(":nth-child(2) > .appointment__card > .appointment__card-left > h2.text--regular")
+      .should("text", "Jessica Jones")
+      .get(":nth-child(2) > .appointment__card > .appointment__card-left > .interviewer > .text--regular")
+      .should("text", "Tori Malcolm")
+
   });
 
 });
